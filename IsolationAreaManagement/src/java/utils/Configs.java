5baea4020afcc3816afbb13b4e194d8ada4c0fc5
Thanhduy19 <phamthanhduy1909@gmail.com>
2022-05-22 21:53:20 +0700

    example

--- IsolationAreaManagement/src/java/utils/Configs.java
+++ IsolationAreaManagement/src/java/utils/Configs.java
@@ -10,6 +10,6 @@ public class Configs {
     public static final String DB_NAME = "swp"; //database name
     public static final String DB_USERNAME = "sa"; // username
     public static final String DB_PASSWORD = "123"; // password
-    public static final String PORT_NUMBER = "1433";//localhost
-
+    public static final String PORT_NUMBER = "1433";//localhost 
+    //duy
 }
