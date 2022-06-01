1af72af2bde54ec2ec111f91f95f4f8aab5d22b7
vuhongphuc1 <phucvh18@gmail.com>
2022-06-01 11:08:16 +0700

    fix bug

--- AI1606_Swp/src/java/controller/base/HomeServlet.java
+++ AI1606_Swp/src/java/controller/base/HomeServlet.java
@@ -24,7 +24,6 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 import utils.Notification;
-import static utils.Utils.md5;
 
 public class HomeServlet extends HttpServlet {
 
--- AI1606_Swp/src/java/controller/base/LoginServlet.java
+++ AI1606_Swp/src/java/controller/base/LoginServlet.java
@@ -18,7 +18,6 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 import utils.Notification;
-import static utils.Utils.md5;
 
 public class LoginServlet extends HttpServlet {
 
