fd17c962b485095baf66c009c514bb4c17d89ba8
Thanhduy19 <phamthanhduy1909@gmail.com>
2022-06-08 22:49:29 +0700

    getAllRoom

--- AI1606_Swp/src/java/dao/RoomDAO.java
+++ AI1606_Swp/src/java/dao/RoomDAO.java
@@ -20,7 +20,7 @@ import java.util.logging.Logger;
 public class RoomDAO implements DAO<Room> {
 
     Connection conn = DBcontext.getConnection();
-    
+    AreaDAO area = new AreaDAO();
     /**
      * GET TOTAL BED IN THE SYSTEM
      * 
@@ -42,7 +42,24 @@ public class RoomDAO implements DAO<Room> {
 
     @Override
     public List<Room> parse(String sql) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        try {
+            Statement sttm = conn.createStatement();
+            ResultSet rs = sttm.executeQuery(sql);
+            ArrayList<Room> room = new ArrayList<>();
+            while (rs.next()) {
+                Room r = new Room();
+                r.setRoomId(rs.getInt("room_id"));
+                r.setRoomName(rs.getString("room_name"));
+                r.setBedNumber(rs.getInt("bed_number"));
+                r.setArea(area.get(rs.getInt("area_id")));
+                r.setNote(rs.getString("note"));
+                room.add(r);
+            }
+            return room;
+        } catch (SQLException ex) {
+            Logger.getLogger(RoomDAO.class.getName()).log(Level.SEVERE, sql, ex);
+        }
+        return null;
     }
 
     @Override
@@ -52,7 +69,10 @@ public class RoomDAO implements DAO<Room> {
 
     @Override
     public List<Room> getAll() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        String sql = "SELECT * FROM room";
+        List<Room> room = new ArrayList<>();
+        room = parse(sql);
+        return room;
     }
 
     @Override
