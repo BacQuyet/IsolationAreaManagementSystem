421a159e99f295ef751023efa9a1d17bbd8e505f
Thanhduy19 <phamthanhduy1909@gmail.com>
2022-06-21 20:53:13 +0700

    update ok

--- AI1606_Swp/src/java/Room/ViewRoomController.java
+++ AI1606_Swp/src/java/Room/ViewRoomController.java
@@ -92,7 +92,7 @@ public class ViewRoomController extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        processRequest(request, response);
+        doGet(request, response);
     }
 
     /**
