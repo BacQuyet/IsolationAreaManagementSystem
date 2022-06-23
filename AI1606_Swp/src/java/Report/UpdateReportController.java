f5b914f785b39a1ea6f8fc9e7c6936c0b4e11e85
QuyetNB <nguyenbacquyet@gmail.com>
2022-06-23 20:49:46 +0700

    fix update Report

--- AI1606_Swp/src/java/Report/UpdateReportController.java
+++ AI1606_Swp/src/java/Report/UpdateReportController.java
@@ -8,6 +8,7 @@ package Report;
 import dao.ReportDAO;
 import java.io.IOException;
 import java.io.PrintWriter;
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -60,6 +61,10 @@ public class UpdateReportController extends HttpServlet {
         response.setContentType("text/html;charset=UTF-8");
         request.setCharacterEncoding("UTF-8");
         ReportDAO dao = new ReportDAO();
+        if (request.getParameter("reportId") == null) {
+            RequestDispatcher update = request.getRequestDispatcher("listReport");
+            update.forward(request, response);
+        }
     }
 
     /**
