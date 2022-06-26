007ecb0ae2b67892b665ad3d0321a8c15bacd739
vuhongphuc1 <phucvh18@gmail.com>
2022-06-26 11:20:03 +0700

    getRequestDispatcher

--- AI1606_Swp/src/java/Doctor/UpdateAccountDoctorController.java
+++ AI1606_Swp/src/java/Doctor/UpdateAccountDoctorController.java
@@ -56,7 +56,8 @@ public class UpdateAccountDoctorController extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        processRequest(request, response);
+        //processRequest(request, response);
+        request.getRequestDispatcher("/myaccount/editAccount.jsp").forward(request, response);
     }
 
     /**
