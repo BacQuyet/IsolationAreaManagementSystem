942da8ade95bc7bdb6d3373891951cb8420b4ebe
vuhongphuc1 <phucvh18@gmail.com>
2022-06-26 11:31:35 +0700

    PreparedStatement

--- AI1606_Swp/src/java/dao/AccountDAO.java
+++ AI1606_Swp/src/java/dao/AccountDAO.java
@@ -68,6 +68,8 @@ public class AccountDAO implements DAO<Account> {
                 + " WHERE account_id =?";
         try {
             PreparedStatement pre = conn.prepareStatement(sql);
+            pre.setString(1, d.getEmail());
+            pre.setInt(2, d.getAccountId());
         } catch (SQLException ex) {
             Logger.getLogger(DoctorDAO.class.getName()).log(Level.SEVERE, sql, ex);
         }
