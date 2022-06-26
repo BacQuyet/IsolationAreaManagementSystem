aa5d0c35b47e4bf69e7ef96a0a6f5c063b6a5925
Thanhduy19 <phamthanhduy1909@gmail.com>
2022-06-26 20:59:40 +0700

    tao ntf

--- AI1606_Swp/src/java/Area/AddAreaController.java
+++ AI1606_Swp/src/java/Area/AddAreaController.java
@@ -70,7 +70,8 @@ public class AddAreaController extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        processRequest(request, response);
+        response.setContentType("text/html;charset=UTF-8");
+        request.setCharacterEncoding("UTF-8");
     }
 
     /**
