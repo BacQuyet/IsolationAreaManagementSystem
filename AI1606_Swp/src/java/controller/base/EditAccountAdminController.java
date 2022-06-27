31ef634e1e6a9ac00ce56edf91eff42259ccdc54
vuhongphuc1 <phucvh18@gmail.com>
2022-06-27 19:16:31 +0700

    getRequestDispatcher

--- AI1606_Swp/src/java/controller/base/EditAccountAdminController.java
+++ AI1606_Swp/src/java/controller/base/EditAccountAdminController.java
@@ -56,7 +56,7 @@ public class EditAccountAdminController extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        processRequest(request, response);
+        request.getRequestDispatcher("/myaccount/editAccount.jsp").forward(request, response);
     }
 
     /**
