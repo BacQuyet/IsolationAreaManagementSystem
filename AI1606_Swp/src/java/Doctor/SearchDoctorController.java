3d46488c73edaf255b4a06c9cc425f4af79bac3f
vuhongphuc1 <phucvh18@gmail.com>
2022-07-12 19:32:14 +0700

    SearchDoctorController

--- AI1606_Swp/src/java/Doctor/SearchDoctorController.java
+++ AI1606_Swp/src/java/Doctor/SearchDoctorController.java
@@ -77,7 +77,30 @@ public class SearchDoctorController extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        response.setCharacterEncoding("UTF-8");
+        request.setCharacterEncoding("UTF-8");
+        int page = 1;
+        int recordsPerPage = 5;
+        String key = request.getParameter("key");
+            if (key == null || "".equals(key)) {
+                response.sendRedirect("ViewDoctor");
+            } else {
+                if (request.getParameter("page") != null) {
+                    page = Integer.parseInt(request.getParameter("page"));
+                }
+                DoctorDAO dao = new DoctorDAO();
+                List<Doctor> list = dao.SearchDoctorByKey(key,(page - 1) * recordsPerPage, recordsPerPage);
+                int noOfRecords = dao.countPageSize(key);
+                int noOfPages = (int) ((noOfRecords + 4) / 5);
+                request.setAttribute("noOfRecords", noOfRecords);
+                request.setAttribute("list", list);
+                request.setAttribute("noOfPages", noOfPages);
+                request.setAttribute("currentPage", page);
+                RequestDispatcher view = request.getRequestDispatcher("list-doctor.jsp");
+                view.forward(request, response);
 
+            }
 
     }
 
