d0a6d623d5973ab7ed55ae649b35c91632e8890d
QuyetNB <nguyenbacquyet@gmail.com>
2022-07-12 22:54:41 +0700

    add entity feedback

---
+++ AI1606_Swp/src/java/entity/Feedback.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package entity;
+
+/**
+ *
+ * @author ADMIN
+ */
+public class Feedback {
+    private String name;
+    private String content;
+    private int patient;
+    
+}
