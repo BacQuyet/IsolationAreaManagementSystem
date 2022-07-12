ad89fd880188fa04e254a3562e7e33fa41945e74
vuhongphuc1 <phucvh18@gmail.com>
2022-07-12 19:39:15 +0700

    fix controller

--- AI1606_Swp/src/java/Patient/SearchPatientController.java
+++ AI1606_Swp/src/java/Patient/SearchPatientController.java
@@ -60,7 +60,8 @@ public class SearchPatientController extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        processRequest(request, response);
+        //processRequest(request, response);
+        doPost(request, response);
     }
 
     /**
