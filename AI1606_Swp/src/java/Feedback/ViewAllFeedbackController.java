b1fb5292f8a5776af6216cc4381b619584c22aac
vuhongphuc1 <phucvh18@gmail.com>
2022-07-13 21:21:13 +0700

    viewAllFeedbackController

--- AI1606_Swp/src/java/Feedback/ViewAllFeedbackController.java
+++ AI1606_Swp/src/java/Feedback/ViewAllFeedbackController.java
@@ -5,8 +5,12 @@
  */
 package Feedback;
 
+import dao.FeedbackDAO;
+import entity.Feedback;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.List;
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -56,7 +60,27 @@ public class ViewAllFeedbackController extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        processRequest(request, response);
+        //processRequest(request, response);
+        response.setContentType("text/html;charset=UTF-8");
+        request.setCharacterEncoding("UTF-8");
+        FeedbackDAO ndao = new FeedbackDAO();
+        int page = 1;
+        int recordsPerPage = 5;
+        if (request.getParameter("page") != null) {
+            page = Integer.parseInt(request.getParameter("page"));
+        }
+        //List<Doctor> list = ndao.getAllDoctor(pageIndex, PAGE_SIZE);
+        List<Feedback> list = ndao.getAllFeedback((page - 1) * recordsPerPage, recordsPerPage);
+        int noOfRecords = ndao.countPage();
+        int noOfPages = (int) ((noOfRecords + 4) / 5);
+
+        request.setAttribute("noOfRecords", noOfRecords);
+        request.setAttribute("list", list);
+        request.setAttribute("noOfPages", noOfPages);
+        request.setAttribute("currentPage", page);
+
+        RequestDispatcher view = request.getRequestDispatcher("/Feedback/listfeedback.jsp");
+        view.forward(request, response);
     }
 
     /**
@@ -70,7 +94,8 @@ public class ViewAllFeedbackController extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        processRequest(request, response);
+        //processRequest(request, response);
+        doGet(request, response);
     }
 
     /**
