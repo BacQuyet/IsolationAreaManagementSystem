12ec82b04c2671988daf93c6ee46319938b2140a
QuyetNB <nguyenbacquyet@gmail.com>
2022-07-14 21:09:41 +0700

    get Parameter page

--- AI1606_Swp/src/java/Feedback/ViewFeedbackController.java
+++ AI1606_Swp/src/java/Feedback/ViewFeedbackController.java
@@ -60,6 +60,9 @@ public class ViewFeedbackController extends HttpServlet {
         request.setCharacterEncoding("UTF-8");
         int page = 1;
         int recordPerPage = 5;
+        if (request.getParameter("page") != null) {
+            page = Integer.parseInt(request.getParameter("page"));
+        }
     }
 
     /**
