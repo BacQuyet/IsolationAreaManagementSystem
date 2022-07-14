8d35bd389ff60cb2b31800391c1d3883f2fdaf0a
QuyetNB <nguyenbacquyet@gmail.com>
2022-07-14 21:43:44 +0700

    delete feedd

--- AI1606_Swp/src/java/Feedback/DeleteFeedbackController.java
+++ AI1606_Swp/src/java/Feedback/DeleteFeedbackController.java
@@ -70,7 +70,8 @@ public class DeleteFeedbackController extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        processRequest(request, response);
+        response.setContentType("text/html;charset=UTF-8");
+        request.setCharacterEncoding("UTF-8");
     }
 
     /**
