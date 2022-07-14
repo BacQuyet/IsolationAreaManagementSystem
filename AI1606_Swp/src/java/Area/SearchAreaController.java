039160ae1645f53f0c28b2615f3b209847e92f23
Thanhduy19 <phamthanhduy1909@gmail.com>
2022-07-14 14:24:52 +0700

    khoi tao page

--- AI1606_Swp/src/java/Area/SearchAreaController.java
+++ AI1606_Swp/src/java/Area/SearchAreaController.java
@@ -56,7 +56,7 @@ public class SearchAreaController extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        processRequest(request, response);
+        doPost(request, response);
     }
 
     /**
@@ -70,7 +70,11 @@ public class SearchAreaController extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        processRequest(request, response);
+        response.setContentType("text/html;charset=UTF-8");
+        request.setCharacterEncoding("UTF-8");
+        
+        int page = 1;
+        int recordPerPage = 5;
     }
 
     /**
