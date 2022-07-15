3d77e46fb6e9a004455de43dd22a0bdf9c00c31d
QuyetNB <nguyenbacquyet@gmail.com>
2022-07-16 01:12:47 +0700

    fix loi chinh ta

--- AI1606_Swp/src/java/Prescription/AddPrescriptionController.java
+++ AI1606_Swp/src/java/Prescription/AddPrescriptionController.java
@@ -84,6 +84,8 @@ public class AddPrescriptionController extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        request.setCharacterEncoding("UTF-8");
         HttpSession session = request.getSession();
         Doctor doctorCurrent = (Doctor)session.getAttribute("doctor");
         String prescriptionName = request.getParameter("prescriptionName");
