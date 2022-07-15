6f276aa4aadf4ca86a725f2ff661bd561ae9b2e7
QuyetNB <nguyenbacquyet@gmail.com>
2022-07-15 23:27:03 +0700

    charset add feed

--- AI1606_Swp/src/java/Feedback/AddFeedbackController.java
+++ AI1606_Swp/src/java/Feedback/AddFeedbackController.java
@@ -56,7 +56,7 @@ public class AddFeedbackController extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        processRequest(request, response);
+        doPost(request, response);
     }
 
     /**
@@ -70,7 +70,7 @@ public class AddFeedbackController extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        processRequest(request, response);
+        response.setContentType("text/html;charset=UTF-8");
     }
 
     /**
