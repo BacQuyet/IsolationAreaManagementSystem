2ae358ca67431d95db20b1948659449f89940dc8
QuyetNB <nguyenbacquyet@gmail.com>
2022-07-15 23:27:29 +0700

    UTF-8

--- AI1606_Swp/src/java/Feedback/AddFeedbackController.java
+++ AI1606_Swp/src/java/Feedback/AddFeedbackController.java
@@ -71,6 +71,7 @@ public class AddFeedbackController extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         response.setContentType("text/html;charset=UTF-8");
+        request.setCharacterEncoding("UTF-8");
     }
 
     /**
